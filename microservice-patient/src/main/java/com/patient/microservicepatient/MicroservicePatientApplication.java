83a3248f5b478ccf735a566b0083d9f248121bc1
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2024-02-01 15:42:19 +0100

    add eureka working with microservice patient

--- microservice-patient/src/main/java/com/patient/microservicepatient/MicroservicePatientApplication.java
+++ microservice-patient/src/main/java/com/patient/microservicepatient/MicroservicePatientApplication.java
@@ -2,8 +2,12 @@ package com.patient.microservicepatient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class MicroservicePatientApplication {
 
 	public static void main(String[] args) {
