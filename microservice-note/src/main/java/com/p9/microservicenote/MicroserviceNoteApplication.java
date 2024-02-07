b4bb5e5cd352318e347d0fb415729f8864353f9f
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2024-02-07 09:12:13 +0100

    extenalize notes properties + link with eureka

--- microservice-note/src/main/java/com/p9/microservicenote/MicroserviceNoteApplication.java
+++ microservice-note/src/main/java/com/p9/microservicenote/MicroserviceNoteApplication.java
@@ -2,8 +2,10 @@ package com.p9.microservicenote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceNoteApplication {
 
 	public static void main(String[] args) {
